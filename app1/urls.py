1477f016a793020c036f7d144dc50dec8531871d
mehraliali1367 <sadadmehrali@gmail.com>
2020-06-07 01:59:44 +0430

    two url changed

--- app1/urls.py
+++ app1/urls.py
@@ -7,5 +7,5 @@ path('',views.index,name='index'),
 path('results/<int:question_id>/',views.results,name='results'),
 path('detail/<int:question_id>/',views.detail),
 path('vote/<int:question_id>/',views.vote),
-path('vote/<int:question_id>/',views.appa),
+path('vote/<int:question_id>/',views.appas),
 ]
